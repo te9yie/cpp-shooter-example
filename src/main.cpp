afde0ad43efdfed4b97ac7b5fb6ec6a467c3dc36
te9yie <te9yie@users.noreply.github.com>
2023-03-08 09:04:35 +0900

    use SDL, imgui

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+int main(int /*argc*/, char* /*argv*/[]) {
+#if defined(_MSC_VER)
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+  SDL_Init(SDL_INIT_VIDEO);
+
+  auto window = SDL_CreateWindow("shooter", SDL_WINDOWPOS_UNDEFINED,
+                                 SDL_WINDOWPOS_UNDEFINED, 16 * 60, 9 * 60,
+                                 SDL_WINDOW_RESIZABLE);
+  auto renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+
+  ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+  ImGui_ImplSDLRenderer_Init(renderer);
+
+  bool loop = true;
+  while (loop) {
+    {
+      SDL_Event e;
+      while (SDL_PollEvent(&e)) {
+        ImGui_ImplSDL2_ProcessEvent(&e);
+        if (e.type == SDL_QUIT) {
+          loop = false;
+        }
+        if (e.type == SDL_WINDOWEVENT &&
+            e.window.event == SDL_WINDOWEVENT_CLOSE &&
+            e.window.windowID == SDL_GetWindowID(window)) {
+          loop = false;
+        }
+      }
+    }
+
+    ImGui_ImplSDLRenderer_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::ShowDemoWindow();
+
+    ImGui::Render();
+
+    auto& io = ImGui::GetIO();
+    SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x,
+                       io.DisplayFramebufferScale.y);
+    SDL_SetRenderDrawColor(renderer, 0x12, 0x34, 0x45, 0xff);
+    SDL_RenderClear(renderer);
+    ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
+    SDL_RenderPresent(renderer);
+  }
+
+  ImGui_ImplSDLRenderer_Shutdown();
+  ImGui_ImplSDL2_Shutdown();
+
+  ImGui::DestroyContext();
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return EXIT_SUCCESS;
+}
---
+++ src/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+
+#include <SDL.h>
+
+#include <cstdlib>
+
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer.h"
